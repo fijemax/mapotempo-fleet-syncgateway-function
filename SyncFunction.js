e4ec89f473ab82b45fced59c50f18c317b0e1f5e
FILLAU Jean-Maxime <jean-maxime.fillau@mapotempo.com>
2017-10-25 11:06:51 +0200

    Double quote -> Simple quote conversion.
    Signed-off-by: FILLAU Jean-Maxime <jean-maxime.fillau@mapotempo.com>

--- SyncFunction.js
+++ SyncFunction.js
@@ -4,7 +4,6 @@ function sync_func(doc, oldDoc) {
     if(doc._deleted && !doc.type && !oldDoc) {
         return;
     }
-
     // ########################
     // ########################
     // ##                    ##
@@ -12,18 +11,16 @@ function sync_func(doc, oldDoc) {
     // ##                    ##
     // ########################
     // ########################
-
     // TYPES CONST
-    var COMPANY = "company";
-    var USER = "user";
-    var MISSION = "mission";
-    var MISSION_STATUS_TYPE = "mission_status_type";
-    var MISSION_STATUS_ACTION = "mission_status_action";
-    var MISSION_STATUS = "mission_status";
-    var TRACK = "track";
-    var CURRENT_LOCATION = "current_location";
-    var METADATA = "metadata";
-
+    var COMPANY = 'company';
+    var USER = 'user';
+    var MISSION = 'mission';
+    var MISSION_STATUS_TYPE = 'mission_status_type';
+    var MISSION_STATUS_ACTION = 'mission_status_action';
+    var MISSION_STATUS = 'mission_status';
+    var TRACK = 'track';
+    var CURRENT_LOCATION = 'current_location';
+    var METADATA = 'metadata';
     // TYPES DRIVER
     var TYPES_DRIVER = {
         company: company,
@@ -31,21 +28,18 @@ function sync_func(doc, oldDoc) {
         mission: mission,
         mission_status_type: mission_status_type,
         mission_status_action: mission_status_action,
-        mission_status: "",
+        mission_status: '',
         track: track,
         current_location: current_location,
-        metadata: ""
+        metadata: ''
     }
-
     // ACTIONS CONST
-    var CREATING = "creating";
-    var UPDATING = "updating";
-    var DELETING = "deleting";
-
+    var CREATING = 'creating';
+    var UPDATING = 'updating';
+    var DELETING = 'deleting';
     // SEPARATOR
-    var ROLE_SEPARATOR = ".";
-    var CHANNEL_SEPARATOR = ":";
-
+    var ROLE_SEPARATOR = '.';
+    var CHANNEL_SEPARATOR = ':';
     // ######################
     // ######################
     // ##                  ##
@@ -53,26 +47,23 @@ function sync_func(doc, oldDoc) {
     // ##                  ##
     // ######################
     // ######################
-
     // *****************************************************************************************************************************************
     // * params : 'type', 'action' and 'role' are accessible for all the functions                                                             *
     // *  type       -> type is a string format extrat from doc.type, it is MANDATORY and it must be one of the key in TYPES_DRIVER            *
-    // *  action     -> "action" is the current action perfomed by the user on the document                                                    *
+    // *  action     -> 'action' is the current action perfomed by the user on the document                                                    *
     // *  company_id -> company_id is a id string format, we can't assure in the sync function that the company really exist in the bucket ... *
-    // *  role       -> role is a string format as that "company_id:type:action" if the action needed a role to be performed it is this        *
+    // *  role       -> role is a string format as that 'company_id:type:action' if the action needed a role to be performed it is this        *
     // *****************************************************************************************************************************************
     var params = {
-        type: "unknow",      // The type of document
-        action: "unknow",    // The action being performed
-        role: "unknow",      // The role need to perform the action
-        company_id: "unknow" // The company id
+        type: 'unknow',      // The type of document
+        action: 'unknow',    // The action being performed
+        role: 'unknow',      // The role need to perform the action
+        company_id: 'unknow' // The company id
     }
-
     params.type = checkAndGetType(doc, oldDoc);
     params.action = checkAndGetAction(doc, oldDoc);
     params.company_id = getCompanyID(doc, oldDoc, params.type);
     params.role = getRoleNeed(params.company_id, params.type, params.action);
-
     // #######################
     // #######################
     // ##                   ##
@@ -80,13 +71,10 @@ function sync_func(doc, oldDoc) {
     // ##                   ##
     // #######################
     // #######################
-
     // Check role and company
     requireRole(params.role);
     checkCompanyID(doc, oldDoc, params.type);
-
     TYPES_DRIVER[params.type](doc, oldDoc, params);
-
     // #######################################
     // #######################################
     // ##                                   ##
@@ -94,13 +82,11 @@ function sync_func(doc, oldDoc) {
     // ##                                   ##
     // #######################################
     // #######################################
-
     // ###############
     // COMPANY MANAGER
     // ###############
     function company(doc, oldDoc, params) {
         var companyChannel = makeCompanyChannel(params.company_id);
-
         switch (params.action) {
             case CREATING:
             case UPDATING:
@@ -113,7 +99,6 @@ function sync_func(doc, oldDoc) {
         // Add current doc in all channels
         channel([companyChannel]);
     }
-
     // ##############
     // USER MANAGER
     // ##############
@@ -128,13 +113,12 @@ function sync_func(doc, oldDoc) {
         doc.channels.push(missionStatusTypeChannel);
         var missionStatusActionChannel = makeMissionStatusActionChannel(params.company_id);
         doc.channels.push(missionStatusActionChannel);
-
         switch (params.action) {
             case CREATING:
             case UPDATING:
                 var userRoles = makeUserRoles(doc, oldDoc, params.company_id);
                 role([sync_user], userRoles);
-      			    access([sync_user], [channelUser]);
+                    access([sync_user], [channelUser]);
                 access([sync_user], [companyChannel]);
                 access([sync_user], [missionStatusTypeChannel]);
                 access([sync_user], [missionStatusActionChannel]);
@@ -143,10 +127,8 @@ function sync_func(doc, oldDoc) {
                 break;
             default:
         }
-
         channel([channelUser]);
     }
-
     // ###############
     // MISSION MANAGER
     // ###############
@@ -154,7 +136,6 @@ function sync_func(doc, oldDoc) {
         // Check owners
         var sync_user = checkSyncUser(doc, oldDoc);
         requireUser(sync_user);
-
         // Check date and make channels
         var date = checkDate(doc, oldDoc);
         var syncUserChannels = makeMissionChannels(sync_user, date);
@@ -171,7 +152,6 @@ function sync_func(doc, oldDoc) {
         // Add current doc in all channels
         channel([syncUserChannels]);
     }
-
     // ######################
     // MISSION STATUS TYPE MANAGER
     // ######################
@@ -180,7 +160,6 @@ function sync_func(doc, oldDoc) {
         // Add current doc in all channels
         channel([missionStatusTypeChannel]);
     }
-
     // ######################
     // MISSION STATUS ACTION MANAGER
     // ######################
@@ -189,13 +168,11 @@ function sync_func(doc, oldDoc) {
         // Add current doc in all channels
         channel([missionStatusActionChannel]);
     }
-
     // ######################
     // TRACK MANAGER
     // ######################
     function track(doc, oldDoc, params) {
     }
-
     // ######################
     // CURRENT LOCATION MANAGER
     // ######################
@@ -210,14 +187,12 @@ function sync_func(doc, oldDoc) {
           default:
       }
     }
-
     // ######################
     // DEFAULT MANAGER
     // ######################
     function default_manager(doc, oldDoc, params) {
       // TODO
     }
-
     // ###################################
     // ###################################
     // ##                               ##
@@ -225,7 +200,6 @@ function sync_func(doc, oldDoc) {
     // ##                               ##
     // ###################################
     // ###################################
-
     // ####################
     // Check Document Field
     // ####################
@@ -234,69 +208,63 @@ function sync_func(doc, oldDoc) {
         var sync_user = oldDoc ? oldDoc.sync_user : doc.sync_user;
         if (!sync_user) {
             throw ({
-                forbidden: "Document must have user"
+                forbidden: 'Document must have user'
             });
         }
         if (Array.isArray(user)) {
             throw ({
-                forbidden: "It can only have one user"
+                forbidden: 'It can only have one user'
             });
         }
         return sync_user;
     }
-
     function checkOwners(doc, oldDoc) {
         // Make sure that the owner propery exists:
         var owners = oldDoc ? oldDoc.owners : doc.owners;
         if (!owners) {
             throw ({
-                forbidden: "Document must have owners"
+                forbidden: 'Document must have owners'
             });
         }
-
         if (!owners.length > 0) {
             throw ({
-                forbidden: "Document must have at least one owner"
+                forbidden: 'Document must have at least one owner'
             });
         }
         return owners;
     }
-
     function checkDate(doc, oldDoc) {
         // Make sure that the checkDeliveryDate propery exists:
         var date = doc ? doc.date : oldDoc.date;
         // On test le cas ou date est null pour prendre celui du oldDoc (pour la suppression)
         date = (!date && oldDoc) ? oldDoc.date : date;
-
         if (!date) {
             throw ({
-                forbidden: "Document must have a date"
+                forbidden: 'Document must have a date'
             });
         }
         if (isNaN(Date.parse(date)))
             throw ({
-                forbidden: "Document must have a date ISO8601 valid format"
+                forbidden: 'Document must have a date ISO8601 valid format'
             });
         return date;
     }
-
     function checkLocation(doc, oldDoc) {
         // Make sure that the location propery exists in the new doc :
         var location = doc.location;
         if (location) {
             if (isNaN(location.lon) || isNaN(location.lat))
                 throw ({
-                    forbidden: "location lat and lon must be a float values"
+                    forbidden: 'location lat and lon must be a float values'
                 });
             else
                 return location;
         } else
             throw ({
-                forbidden: "Document must have a location"
+                forbidden: 'Document must have a location'
             });
         return location;
     }
-
 // REMOVE checkAddress function, this isn't mandatory
 /*
     function checkAddress(doc, oldDoc) {
@@ -305,35 +273,35 @@ function sync_func(doc, oldDoc) {
         if (address) {
             if (!address.street)
                 throw ({
-                    forbidden: "street field in address is mandatory"
+                    forbidden: 'street field in address is mandatory'
                 });
             if (!address.postalcode)
                 throw ({
-                    forbidden: "postalcode field in address is mandatory"
+                    forbidden: 'postalcode field in address is mandatory'
                 });
             if (!address.city)
                 throw ({
-                    forbidden: "city field in address is mandatory"
+                    forbidden: 'city field in address is mandatory'
                 });
             if (!address.state)
                 throw ({
-                    forbidden: "state field in address is mandatory"
+                    forbidden: 'state field in address is mandatory'
                 });
             if (!address.country)
                 throw ({
-                    forbidden: "country field in address is mandatory"
+                    forbidden: 'country field in address is mandatory'
                 });
         } else
             throw ({
-                forbidden: "Document must have an address"
+                forbidden: 'Document must have an address'
             });
         return address;
     }
 */
     function checkName(doc, oldDoc) {
-        if (!doc.name ||  typeof(doc.name) !== "string") {
+        if (!doc.name ||  typeof(doc.name) !== 'string') {
             throw ({
-                forbidden: "Document must have a name"
+                forbidden: 'Document must have a name'
             });
         }
     }
@@ -343,40 +311,33 @@ function sync_func(doc, oldDoc) {
     function makeCompanyChannel(company_id) {
         return COMPANY + CHANNEL_SEPARATOR + company_id;
     }
-
     function makeMissionStatusTypeChannel(company_id) {
         return MISSION_STATUS_TYPE + CHANNEL_SEPARATOR + company_id;
     }
-
     function makeMissionStatusActionChannel(company_id) {
         return MISSION_STATUS_ACTION + CHANNEL_SEPARATOR + company_id;
     }
-
     function makeUserChannel(user) {
         return USER + CHANNEL_SEPARATOR + user;
     }
-
     function makeCurrentLocationChannel(owner) {
         return CURRENT_LOCATION + CHANNEL_SEPARATOR + owner;
     }
-
     function makeMissionChannels(sync_user, date) {
         // Date format yyyyMMdd for channel
         var timestamp = Date.parse(date);
         if (isNaN(timestamp))
             throw ({
-                forbidden: "Document must have a date ISO8601 valid format"
+                forbidden: 'Document must have a date ISO8601 valid format'
             });
         var date = new Date(timestamp);
-        var channel_date = "" + date.getFullYear() +
-            ("0" + (date.getMonth() + 1)).slice(-2) +
-            ("0" + date.getDate()).slice(-2)
-
+        var channel_date = '' + date.getFullYear() +
+            ('0' + (date.getMonth() + 1)).slice(-2) +
+            ('0' + date.getDate()).slice(-2)
         // Create channel patern [type:owner:yyyyMMdd]
         var sync_user_channel = MISSION + CHANNEL_SEPARATOR + sync_user + CHANNEL_SEPARATOR + channel_date;
         return sync_user_channel;
     }
-
     // ######################
     // Action and Type Helper
     // ######################
@@ -385,7 +346,7 @@ function sync_func(doc, oldDoc) {
     
         if (!type || !TYPES_DRIVER[type]) {
             throw ({
-                forbidden: "Unknown document type"
+                forbidden: 'Unknown document type'
             });
         }
         if (oldDoc && doc) {
@@ -393,14 +354,13 @@ function sync_func(doc, oldDoc) {
                 // Check is not a delete action
                 if (doc._deleted !== true) {
                     throw ({
-                        forbidden: "Document \"type\" can't be modify"
+                        forbidden: 'Document type cannot be modify'
                     });
                 }
             }
         }
         return type;
     }
-
     function checkAndGetAction(doc, oldDoc) {
         if (!oldDoc)
             return CREATING;
@@ -409,56 +369,48 @@ function sync_func(doc, oldDoc) {
         else
             return UPDATING;
     }
-
     // #################
     // company_id Helper
     // #################
-
     function getCompanyID(doc, oldDoc, type) {
-        if (type === "company")
+        if (type === 'company')
             return oldDoc ? oldDoc._id : doc._id;
         else
             return oldDoc ? oldDoc.company_id : doc.company_id;
     }
-
     function checkCompanyID(doc, oldDoc, type) {
-        if (type === "company")
+        if (type === 'company')
             return;
-
         var company_id = oldDoc ? oldDoc.company_id : doc.company_id;
         if (!company_id) {
             throw ({
-                forbidden: "Document must have a company_id"
+                forbidden: 'Document must have a company_id'
             });
         }
-
         // Make sure that nobody can modify company id.
         if (doc && oldDoc)
             if (doc.company_id && oldDoc.company_id)
                 if (doc.company_id != oldDoc.company_id) {
                     throw ({
-                        forbidden: "Document ID can't be modify"
+                        forbidden: 'Document ID cannot be modify'
                     });
                 }
     }
-
     // ###########
     // Role Helper
     // ###########
-
     function getRoleNeed(company_id, type, action) {
         return company_id + ROLE_SEPARATOR + type + ROLE_SEPARATOR + action;
     }
-
     function makeUserRoles(doc, oldDoc, company_id) {
         var roles = oldDoc ? oldDoc.roles : doc.roles;
         var res = [];
         if(roles && Array.isArray(roles)) {
           for (var i = 0; i < roles.length; i++) {
-              res[i] = "role:" + company_id + ROLE_SEPARATOR + roles[i];
+              res[i] = 'role:' + company_id + ROLE_SEPARATOR + roles[i];
           }
         } else {
-            res[0] = "role:default";
+            res[0] = 'role:default';
         }
         return res;
     }
